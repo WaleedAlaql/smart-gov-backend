9b35e87d62295a0de8845aee15ad95b17097e7a3
Waleed Alaql <waleedalaql10@gmail.com>
2024-11-21 20:26:30 +0300

    Initial Start

---
+++ smart-gov/src/main/java/com/waleed/smart_gov/SmartGovApplication.java
@@ -0,0 +1,13 @@
+package com.waleed.smart_gov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartGovApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartGovApplication.class, args);
+	}
+
+}
